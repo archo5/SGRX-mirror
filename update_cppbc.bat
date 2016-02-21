72c0b4f87f4b5ca919e6c56eb128427fffb60706
snake5 <snake5@89f9ca44-2ff2-4b64-a49d-2a4f1987ee72>
2016-02-21 17:03:19 +0000

    some cleanup, part 1
    git-svn-id: http://svn.sgscript.org/tacstrike@597 89f9ca44-2ff2-4b64-a49d-2a4f1987ee72

--- update_cppbc.bat
+++ update_cppbc.bat
@@ -4,13 +4,13 @@
 sgsvm -p ../../ext/src/sgscript/cppbc.sgs gfwcore.hpp
 sgsvm -p ../../ext/src/sgscript/cppbc.sgs scritem.hpp
 sgsvm -p ../../ext/src/sgscript/cppbc.sgs gamegui.hpp
+sgsvm -p ../../ext/src/sgscript/cppbc.sgs level.hpp
+sgsvm -p ../../ext/src/sgscript/cppbc.sgs entities.hpp -i level.hpp
+sgsvm -p ../../ext/src/sgscript/cppbc.sgs systems.hpp -i level.hpp
 @popd
 @pushd src\tacstrike
 @echo.
 @echo in src/tacstrike
-sgsvm -p ../../ext/src/sgscript/cppbc.sgs level.hpp
-sgsvm -p ../../ext/src/sgscript/cppbc.sgs entities.hpp -i level.hpp
-sgsvm -p ../../ext/src/sgscript/cppbc.sgs systems.hpp -i level.hpp
-sgsvm -p ../../ext/src/sgscript/cppbc.sgs entities_ts.hpp -i level.hpp
-sgsvm -p ../../ext/src/sgscript/cppbc.sgs isr3entities.hpp -i level.hpp
+sgsvm -p ../../ext/src/sgscript/cppbc.sgs entities_ts.hpp -i ../common/level.hpp
+sgsvm -p ../../ext/src/sgscript/cppbc.sgs isr3entities.hpp -i ../common/level.hpp
 @popd
