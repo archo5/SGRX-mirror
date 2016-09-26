a1e529e116bebfc62ed5d2da3e3221b0c8e637df
snake5 <snake5@89f9ca44-2ff2-4b64-a49d-2a4f1987ee72>
2016-09-26 19:24:01 +0000

    finished lev1 whiteboxing, some AI experiments
    git-svn-id: http://svn.sgscript.org/tacstrike@916 89f9ca44-2ff2-4b64-a49d-2a4f1987ee72

--- src/common/mapedit_gameobj.cpp
+++ src/common/mapedit_gameobj.cpp
@@ -300,13 +300,13 @@ GameObject* EDGO_FLoad( sgsVariable data )
 		SGRX_GUID guid = SGRX_GUID::ParseString(
 			data_bhvr.getprop( "__guid" ).get<StringView>() );
 		g_Level->nextObjectGUID = guid.NotNull() ? guid : SGRX_GUID::Generate();
-		LOG << "BHVR" << guid << g_Level->nextObjectGUID;
+	//	LOG << "BHVR" << guid << g_Level->nextObjectGUID;
 		
 		GOBehavior* bhvr = obj->RequireBehavior( name, type, true );
-		if(bhvr)
-			LOG << "---FINL" << bhvr->m_src_guid;
-		else
-			LOG << "----NO BHVR????" << name.c_str() << "|" << type.c_str();
+	//	if(bhvr)
+	//		LOG << "---FINL" << bhvr->m_src_guid;
+	//	else
+	//		LOG << "----NO BHVR????" << name.c_str() << "|" << type.c_str();
 		if( bhvr == NULL )
 		{
 			LOG_ERROR << "FAILED TO LOAD BEHAVIOR type="
